ab3dc08c95e312cd24f4b6fd10c7c6ebd592161e
Mohdaman01 <mohdastar200026@gmail.com>
2025-10-08 12:00:12 +0530

    changed enpoint from catalog-plugin to get-catalog-items

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @methodType {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    WIX_APP_JWT_KEY: process.env.WIX_APP_JWT_KEY,
+    WIX_APP_ID: process.env.WIX_APP_ID,
+    WIX_APP_SECRET: process.env.WIX_APP_SECRET,
+  },
 };
 
 module.exports = nextConfig;
