7337a4ef114e40663b3cd797dc17b0bf3c65c9cf
Mohdaman01 <mohdastar200026@gmail.com>
2025-12-10 15:35:54 +0530

    error handling in uninstall webhook

--- src/app/api/webhooks/v1/uninstall/route.ts
+++ src/app/api/webhooks/v1/uninstall/route.ts
@@ -2,18 +2,25 @@ import { NextRequest } from 'next/server';
 import { wixAppClient } from '@/app/utils/wix-sdk.app';
 
 export async function POST(request: NextRequest) {
-  console.info('Webhook::uninstall - called');
-  const { eventType, instanceId, payload } = await wixAppClient.webhooks.processRequest(request, {
-    expectedEvents: [wixAppClient.webhooks.apps.AppRemoved],
-  });
+  try {
+    console.info('Webhook::uninstall - called');
+    const { eventType, instanceId, payload } = await wixAppClient.webhooks.processRequest(request, {
+      expectedEvents: [wixAppClient.webhooks.apps.AppRemoved],
+    });
 
-  console.info('Webhook::uninstall - input is:', {
-    eventType,
-    instanceId,
-    payload,
-  });
+    console.info('Webhook::uninstall - input is:', {
+      eventType,
+      instanceId,
+      payload,
+    });
 
-  return new Response('OK', {
-    status: 200,
-  });
+    return new Response('OK', {
+      status: 200,
+    });
+  } catch (err) {
+    console.error('Webhook::uninstall - error occurred:', err);
+    return new Response('Internal Server Error', {
+      status: 500,
+    });
+  }
 }
